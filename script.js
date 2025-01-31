e052e98a340ed2fd7d9c4ba72330cf3027e6615c
Durgesh kushwaha <durgeshkushwaha415@gamil.com>
2025-01-31 15:43:46 +0530

    completed

--- script.js
+++ script.js
@@ -1,3 +1,57 @@
+const scroller = new LocomotiveScroll({
+    el: main,
+    inertia: 0.8,
+    smooth: true,
+    getDirection: true,
+    mobile: {
+      smooth: true,
+      inertia: 0.8,
+      getDirection: true,
+    },
+    tablet: {
+      smooth: true,
+      inertia: 0.8,
+      getDirection: true,
+    },
+  });
+
+
+function locomotive(){
+  
+    gsap.registerPlugin(ScrollTrigger);
+    
+    // Using Locomotive Scroll from Locomotive https://github.com/locomotivemtl/locomotive-scroll
+    
+    const locoScroll = new LocomotiveScroll({
+      el: document.querySelector("#main"),
+      smooth: true
+    });
+    // each time Locomotive Scroll updates, tell ScrollTrigger to update too (sync positioning)
+    locoScroll.on("scroll", ScrollTrigger.update);
+    
+    // tell ScrollTrigger to use these proxy methods for the "#main" element since Locomotive Scroll is hijacking things
+    ScrollTrigger.scrollerProxy("#main", {
+      scrollTop(value) {
+        return arguments.length ? locoScroll.scrollTo(value, 0, 0) : locoScroll.scroll.instance.scroll.y;
+      }, // we don't have to define a scrollLeft because we're only scrolling vertically.
+      getBoundingClientRect() {
+        return {top: 0, left: 0, width: window.innerWidth, height: window.innerHeight};
+      },
+      // LocomotiveScroll handles things completely differently on mobile devices - it doesn't even transform the container at all! So to get the correct behavior and avoid jitters, we should pin things with position: fixed on mobile. We sense it by checking to see if there's a transform applied to the container (the LocomotiveScroll-controlled element).
+      pinType: document.querySelector("#main").style.transform ? "transform" : "fixed"
+    });
+    
+    
+    
+    // each time the window updates, we should refresh ScrollTrigger and then update LocomotiveScroll. 
+    ScrollTrigger.addEventListener("refresh", () => locoScroll.update());
+    
+    // after everything is set up, refresh() ScrollTrigger and update LocomotiveScroll because padding may have been added for pinning, etc.
+    ScrollTrigger.refresh();
+    
+    }
+    locomotive();
+
 
 function loadingAnimation() {
     var tl = gsap.timeline()
@@ -105,7 +159,91 @@ function page2Animation() {
     })
 }
 
-page2Animation()
+
+gsap.to(".page6 h1", {
+    x: "-200%", // Use 'x' instead of 'transform'
+    scrollTrigger: {
+      trigger: ".page6",
+      scroller:"#main",
+    //   markers: true,
+      start: "top 60%",
+      end: "top 10%",
+      scrub:2,
+    }
+  });
+  gsap.from(".page7 h1", {
+    x: "-200%", // Use 'x' instead of 'transform'
+    scrollTrigger: {
+      trigger: ".page7",
+      scroller:"#main",
+    //   markers: true,
+      start: "top 78%",
+      end: "top 10%",
+      scrub:2,
+    }
+  });
+  gsap.to(".page8 h1", {
+    x: "-200%", // Use 'x' instead of 'transform'
+    scrollTrigger: {
+      trigger: ".page8",
+      scroller:"#main",
+    //   markers: true,
+      start: "top 95%",
+      end: "top 10%",
+      scrub:2,
+    }
+  });
+
+  var footText = document.querySelectorAll("#footer .text")
+
+  footText.forEach(function(elem){
+      var elemText  = elem.textContent
+      var splited = elemText.split("")
+      var clutter = ""
+     
+      splited.forEach(function(e){
+          clutter += `<span>${e}</span>`
+     
+      });
+      elem.innerHTML = clutter
+
+  });
+ 
+  
+  var footerText = document.querySelector(".footer-text")
+  
+  footerText.addEventListener("mouseenter",function(){
+      gsap.to("#footer h1 span",{
+          opacity:0,
+          delay:0.,
+          duration:0.2,
+          stagger:0.03
+      })
+      gsap.to("#footer h2 span",{
+          opacity:1,
+          delay:0.,
+          duration:0.2,
+          stagger:0.04
+      })
+  })
+  
+  footerText.addEventListener("mouseleave",function(){
+      gsap.to("#footer h2 span",{
+          opacity:0,
+          stagger:0.05,
+          duration:0.3
+      })
+      gsap.to("#footer h1 span",{
+          opacity:1,
+          delay:0.4,
+          duration:0.3,
+          stagger:0.03
+      })
+  })
+
+
+
+page2Animation();
 
 
 function SheryAnimation(){
@@ -123,4 +261,4 @@ function SheryAnimation(){
 }
 
 
-SheryAnimation()
+SheryAnimation();
